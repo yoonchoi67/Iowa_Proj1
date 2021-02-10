3e26e6f62ede93ea9818c8598f0d6a7e85e2ebab
Yoon Choi <choijong@grinnell.edu>
2021-02-10 15:55:29 +0900

    change directory format and remove unncessary stuff

--- xml_to_dict.py
+++ xml_to_dict.py
@@ -5,18 +5,18 @@ import formatDrugNames
 #================================================================================#
 
 # create an outfile.json output file if not exists
-if not os.path.exists('outfile.json'):
-    open('outfile.json', 'w').close()
+if not os.path.exists('data/outfile.json'):
+    open('data/outfile.json', 'w').close()
 
 # convert SearchResults.xml to outfile.json if it hadn't been done before
-if os.stat("outfile.json").st_size == 0:
-    with open('SearchResults.xml', 'r', encoding='utf-8') as myfile:
-        with open('outfile.json', 'w', encoding='utf-8') as outfile:
+if os.stat("data/outfile.json").st_size == 0:
+    with open('data/SearchResults.xml', 'r', encoding='utf-8') as myfile:
+        with open('data/outfile.json', 'w', encoding='utf-8') as outfile:
             obj = xmltodict.parse(myfile.read())
             json.dump(obj, outfile, indent=4, sort_keys=True)
 
 # json load
-f=open('outfile.json')
+f=open('data/outfile.json')
 data=json.load(f)
 
 #================================================================================#
@@ -30,9 +30,9 @@ store_results = dict()
 # do analysis on each study through json format
 for each in data['search_results']['study'][:4]:
     # Use formatDrugNames.py and change the dictionary
-    for inv in each['interventions']['intervention']:
-        changed = formatDrugNames.formatDrugName(inv['#text'])
-        print(changed)
+    # for inv in each['interventions']['intervention']:
+    #     changed = formatDrugNames.formatDrugName(inv['#text'])
+    #     print(changed)
 
     # put into final dict, store_results
     store_results[each['@rank']]=each
