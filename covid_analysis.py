8ce65a9631af76439526cc684d3ed12ae3fb5f88
Yoon Choi <choijong@grinnell.edu>
2021-02-15 11:28:35 +0900

    change def order around & change type comparison and & remove commas

--- covid_analysis.py
+++ covid_analysis.py
@@ -7,7 +7,7 @@ def parseXML(xmlfile):
     root = tree.getroot()
     tag = root.tag
     phases_dictionary = {}
-    study_counter = 0;
+    study_counter = 0
     for elem in root.iter():
         
         
@@ -15,7 +15,7 @@ def parseXML(xmlfile):
             study_counter+=1
             print(elem.attrib)
         if elem.tag == 'phases':
-            counter = 0;
+            counter = 0
             for child in elem:
                 print(child.text)
                 counter = counter + 1
@@ -30,10 +30,75 @@ def parseJSON(jsonfile):
     phase_dict = getPhaseData(data)
     status_dict = getActivityStatus(data)
     intervention_dict, num_intervention = getInterventionStatus(data)
-    print(phase_dict)
-    print(status_dict)
-    print(intervention_dict)
-    print(num_intervention)
+    print(phase_dict, "\n")
+    print(status_dict, "\n")
+    print(intervention_dict, "\n")
+    print(num_intervention, "\n")
+
+    
+def getPhaseData(data):
+    phases_dictionary = {}
+    countNone = 0
+    countMulti = 0
+    countSingle = 0
+    
+    # do analysis on each study through json format
+    for study in data['search_results']['study']:
+        
+        # number of studies with no phases
+        if type(study['phases']) is type(None):
+            countNone+=1
+            if not "None" in phases_dictionary:
+                phases_dictionary["None"] = countNone
+            else:
+                phases_dictionary["None"] += 1
+
+        else:
+            for phase in study['phases']:
+                #print(type(study['phases'][phase]))
+                if type(study['phases'][phase]) is str:
+                    countSingle+=1
+                    p = study['phases'][phase]
+                    if not p in phases_dictionary:
+                        phases_dictionary[p] = 1
+                    else:
+                        phases_dictionary[p] += 1
+                else:
+                    countMulti+=1
+                    sep = '/'
+                    p = sep.join(study['phases'][phase])
+                    if not p in phases_dictionary:
+                        phases_dictionary[p] = 1
+                    else:
+                        phases_dictionary[p] += 1
+
+    #print(countNone)
+    #print(countMulti)
+    #print(countSingle)
+    #print(phases_dictionary)
+    return phases_dictionary
+
+
+def getActivityStatus(data):
+    status_dict = {}
+    for study in data['search_results']['study']:
+        #print(type(study['status']))
+        #print(study['status'])
+        status = []
+        sep = ', '
+        for key in study['status']:
+            if study['status'][key] == 'N':
+                status.append("Not Open")
+            elif study['status'][key] == 'Y':
+                status.append("Open")
+            else:
+                status.append(study['status'][key])
+        p = sep.join(status)
+        if not p in status_dict:
+                status_dict[p] = 1
+        else:
+                status_dict[p] += 1
+    return status_dict
 
 
 def getInterventionStatus(data):
@@ -50,7 +115,7 @@ def getInterventionStatus(data):
                 num_intervention_dict[0] += 1
         else:
             
-            if str(type(study['interventions']['intervention']))=="<type 'dict'>":
+            if type(study['interventions']['intervention']) is dict:
                 if not 1 in num_intervention_dict:
                     num_intervention_dict[1] = 1
                 else:
@@ -74,73 +139,6 @@ def getInterventionStatus(data):
  
     return intervention_dict, num_intervention_dict
     
-def getActivityStatus(data):
-    status_dict = {};
-    for study in data['search_results']['study']:
-        #print(type(study['status']))
-        #print(study['status'])
-        status = [];
-        sep = ', '
-        for key in study['status']:
-            if study['status'][key] == 'N':
-                status.append("Not Open")
-            elif study['status'][key] == 'Y':
-                status.append("Open")
-            else:
-                status.append(study['status'][key])
-        p = sep.join(status)
-        if not p in status_dict:
-                status_dict[p] = 1
-        else:
-                status_dict[p] += 1
-    return status_dict
-    
-def getPhaseData(data):
-    phases_dictionary = {}
-    countNone = 0;
-    countMulti = 0
-    countSingle = 0;
-    
-    # do analysis on each study through json format
-    for study in data['search_results']['study']:
-        
-        if type(study['phases']) is type(None):
-            countNone+=1
-            if not "None" in phases_dictionary:
-                phases_dictionary["None"] = countNone
-            else:
-                phases_dictionary["None"] += 1
-
-        else:
-            for phase in study['phases']:
-                #print(type(study['phases'][phase]))
-                if str(type(study['phases'][phase]))=="<type 'unicode'>":
-                    countSingle+=1
-                    p = study['phases'][phase]
-                    if not p in phases_dictionary:
-                        phases_dictionary[p] = 1
-                    else:
-                        phases_dictionary[p] += 1
-                else:
-                    countMulti+=1
-                    sep = '/'
-                    p = sep.join(study['phases'][phase])
-                    if not p in phases_dictionary:
-                        phases_dictionary[p] = 1
-                    else:
-                        phases_dictionary[p] += 1
-
-
-                
-                
-
-    #print(countNone)
-    #print(countMulti)
-    #print(countSingle)
-    #print(phases_dictionary)
-    return phases_dictionary
-
-
       
 def main():
     parseJSON('data/outfile.json')
