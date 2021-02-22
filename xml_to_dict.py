b0c7889d864e93c902f771d85de3aba340557aea
Yoon Choi <choijong@grinnell.edu>
2021-02-22 11:28:49 +0900

    unzip

--- xml_to_dict.py
+++ xml_to_dict.py
@@ -19,6 +19,8 @@ if os.stat("data/outfile.json").st_size == 0:
 f=open('data/outfile.json')
 data=json.load(f)
 
+#================================================================================#
+
 # create an COVIDoutfile.json (COVID Results) output file if not exists
 if not os.path.exists('data/COVIDoutfile.json'):
     open('data/COVIDoutfile.json', 'w').close()
@@ -35,3 +37,18 @@ f=open('data/COVIDoutfile.json')
 data=json.load(f)
 
 #================================================================================#
+
+# # create an covidAdverseFile.json (COVID Results) output file if not exists
+# if not os.path.exists('data/covidAdverse/COVIDoutfile.json'):
+#     open('data/COVIDoutfile.json', 'w').close()
+
+# # convert SearchResults.xml to outfile.json if it hadn't been done before
+# if os.stat("data/COVIDoutfile.json").st_size == 0:
+#     with open('data/COVIDSearchResults.xml', 'r', encoding='utf-8') as myfile:
+#         with open('data/COVIDoutfile.json', 'w', encoding='utf-8') as outfile:
+#             obj = xmltodict.parse(myfile.read())
+#             json.dump(obj, outfile, indent=4, sort_keys=True)
+
+# # json load
+# f=open('data/COVIDoutfile.json')
+# data=json.load(f)
