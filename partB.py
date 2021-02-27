fa176e77b70ed53e155982db1b9225aa0ec54f59
Yoon Choi <choijong@grinnell.edu>
2021-02-27 21:04:46 +0900

    intermediate progress

--- partB.py
+++ partB.py
@@ -2,6 +2,7 @@ import xml.etree.ElementTree as ET
 import json
 import datetime
 from dateutil.parser import parse
+import matplotlib.pyplot as plt
 
 def getMostStudiedDrugs(data):
     """get 15 most studied drugs that appeared in trials for each disease
@@ -47,11 +48,75 @@ def getAdverseEvents(data):
     mostAdverseEvents={}
     return mostAdverseEvents
 
+def getTrends(data):
+    """get study duration of the studies
+    Args: data: dict
+    Returns: duration_dict: dict
+    """
+    start_year_dict = {}
+    end_year_dict = {}
+    anytime_dict = {}
+
+    duration_dict = {}
+    
+    for study in data['search_results']['study']:
+        startdate = ''
+        end = ''
+        if not 'completion_date' in study:
+            if 'primary_completion_date' in study:
+                end = parse(study['primary_completion_date'])
+            elif 'last_update_posted' in study:
+                end = parse(study['last_update_posted'])
+        else:
+            end = parse(study['completion_date'])
+        
+        if not 'start_date' in study:
+            if 'study_first_posted' in study:
+                startdate = parse(study['study_first_posted'])
+        else:
+            startdate = parse(study['start_date'])
+
+        #update start_year_dict
+        if startdate.year not in start_year_dict: start_year_dict[startdate.year] = 1
+        else: start_year_dict[startdate.year] += 1
+        
+        #update end_year_dict
+        if end.year not in end_year_dict: end_year_dict[end.year] = 1
+        else: end_year_dict[end.year] += 1
+        
+        #update anytime_dict
+        duration = end.year - startdate.year + 1
+        for i in range(duration):
+            yr = startdate.year + i
+            if yr not in anytime_dict: anytime_dict[yr] = 1
+            else: anytime_dict[yr] += 1
+
+    return start_year_dict, anytime_dict, end_year_dict
+
 def getAllPartB(data):
     """Get Part B"""
     mostStudiedDrugsDict = getMostStudiedDrugs(data)
     mostAdverseEvents = getAdverseEvents(data)
+    start, anytime, end = getTrends(data)
 
     """Print Part B"""
-    print("Most Studied Drugs and their frequencies: ", mostStudiedDrugsDict, "\n")
-    print("Most Serious Adverse Events that occurred: ", mostAdverseEvents)
+    # print("Most Studied Drugs and their frequencies: ", mostStudiedDrugsDict, "\n")
+    # print("Most Serious Adverse Events that occurred: ", mostAdverseEvents, "\n")
+    print("Number of trials started at each year: ", start, "\n")
+    print("Number of trials ongoing at any year: ", anytime, "\n")
+    print("Number of trials ended at each year: ", end, "\n")
+    
+    """ Show Bar Graph """
+    fig = plt.figure()
+    plt.bar(range(len(start)), list(start.values()), align='center')
+    plt.xticks(range(len(start)), list(start.keys()))
+    fig.suptitle('Start year')
+    plt.xlabel('year')
+    plt.ylabel('frequency')
+    plt.show()
+
+    """ Show Box Chart """
+    # not sure if we need this
+    x = [k for k, v in start.items()]
+    plt.boxplot(x)
+    plt.show()
